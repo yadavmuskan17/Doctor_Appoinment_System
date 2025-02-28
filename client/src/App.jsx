70e3d3743a4de2ae6144a86b3651b99a188231c2
yadavmuskan17 <yadavmuskan1704@gmail.com>
2025-03-01 00:53:30 +0530

    doctor app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,6 @@ import Layout from "./Layout";
 import Home from "./pages/Home";
 import DoctorLogin from "./pages/DoctorLogin";
 import DoctorDashBoard from "./DoctorDashBoard";
-import SearchDoctor from "./pages/SearchDoctor";
 import PatientAppointment from "./pages/PatientAppointment";
 import MyPatient from "./pages/MyPatient";
 import Signup from "./pages/Signup";
@@ -16,7 +15,6 @@ const App=()=>{
           <Route index element={<Home/>} />
           <Route path="doctorlogin" element={<DoctorLogin/>}/>
           <Route path="signup" element={<Signup/>}></Route>
-          <Route path="searchdoctor" element={<SearchDoctor/>} />
           <Route path="patientapp/:id" element={<PatientAppointment/>} />
         </Route>
       </Routes>
